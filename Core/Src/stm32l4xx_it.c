5172ddf107db093ff5ab90c92d58326e628ae052
Gabs <gromo@unal.edu.co>
2024-09-28 21:47:16 -0500

    Corrections #2

--- Core/Src/stm32l4xx_it.c
+++ Core/Src/stm32l4xx_it.c
@@ -288,7 +288,7 @@ void USART3_IRQHandler(void)
 {
   /* USER CODE BEGIN USART3_IRQn 0 */
 
-#if 0
+#if 1
   /* USER CODE END USART3_IRQn 0 */
   HAL_UART_IRQHandler(&huart3);
   /* USER CODE BEGIN USART3_IRQn 1 */
